ec317fcb1142e3dfbecc1da4d4351e59dcdad75c
karida-julia <karidajulia1@gmail.com>
2025-10-03 11:48:48 -0300

    Add files via upload

---
+++ Front/script.js
@@ -0,0 +1,37 @@
+const resultsDiv = document.getElementById("results")
+
+async function fetchFruits(){
+    resultsDiv.innerHTML = "<p>Carregando...</p>"
+
+    try {
+        const response = await fetch('../Dados/data.json')
+        const data = await response.json()
+       // console.log(data)
+
+       if(data.error){
+         resultsDiv.innerHTML = "<p>Erro ao buscar produtos!</p>"
+         return
+       }
+       resultsDiv.innerHTML =''
+
+       data.forEach(item =>{
+        const card = document.createElement("div")
+        card.className = "card"
+        card.innerHTML = `
+            <img src="${item.imagem}" alt="${item.nome}"
+            <h2>${item.nome}</h2>
+            <h3>${item.descricao}</h3>
+            <h4>${item.categoria}</h4>
+            <p>${item.setor}</p>
+        `
+    
+        resultsDiv.appendChild(card)
+       })
+    } catch (error) {
+        resultsDiv.innerHTML = "<p>Erro ao buscar produtos!</p>"
+        
+    }
+
+}
+
+fetchFruits()
